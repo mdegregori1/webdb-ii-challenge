eb830985912e4aa1772ab16456df0233de896595
Mauricio Degregori <degregori2@gmail.com>
2019-12-10 16:28:29 -0600

    table made + car-dealer.db3 made + knexfile completed

---
+++ data/migrations/20191210153404_addCarsTable.js
@@ -0,0 +1,34 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('car-dealer', tbl => {
+    // critical
+    tbl.increments();
+    tbl.string('VIN', 17)
+      .notNullable()
+      .unique()
+      .index()
+    tbl.string('make', 150)
+      .notNullable()
+      .index(); 
+    tbl.string('model', 150)
+      .notNullable()
+      .index();// makes searching for vegetables using this column faster
+
+    tbl.integer('mileage', 6)
+      .notNullable()
+      .index()    
+
+      // not required
+    tbl.boolean('automatic').defaultTo(true)
+    tbl.string('status of title ', 150)
+      
+})
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('car-dealer')
+  };
+  
+
+// The critical information for each car is the VIN, make, model, and mileage.
+// - They also track transmission type and status of the title (clean, salvage, etc.), but this information is not always immediately known.
---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// this file contains instructions knex needs to connect to the db
+
+module.exports = {
+  // teaches knex to connect to the development database
+  development: {
+    client: 'sqlite3', // the DB driver
+    useNullAsDefault: true, //only needed when using SQLite3
+    connection: {
+      filename: './data/car-dealer.db3'
+    },
+    migrations: {
+      directory: './data/migrations' // will be created automatically (same for seeds)
+    }
+  },
+};
